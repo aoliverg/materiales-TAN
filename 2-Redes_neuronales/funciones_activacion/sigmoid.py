9a5cea803a71ac5a585da79fdf2851cc4bc13534
aoliverg <aoliverg@uoc.edu>
2023-10-15 14:13:06 +0200

    Add files via upload

---
+++ 2-Redes_neuronales/funciones_activacion/sigmoid.py
@@ -0,0 +1,15 @@
+# Sigmoid function in Python
+# from: https://www.datacamp.com/tutorial/pytorch-tutorial-building-a-simple-neural-network-from-scratch
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-5, 5, 50)
+z = 1/(1 + np.exp(-x))
+
+plt.subplots(figsize=(8, 5))
+plt.plot(x, z)
+plt.grid()
+plt.title("Sigmoide")
+plt.ylabel("Activación F(x)")
+plt.xlabel("Estímulo x")
+plt.show()
---
+++ 2-Redes_neuronales/funciones_activacion/tanh.py
@@ -0,0 +1,15 @@
+# tanh function in Python
+# from: https://www.datacamp.com/tutorial/pytorch-tutorial-building-a-simple-neural-network-from-scratch
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-5, 5, 50)
+z = np.tanh(x)
+
+plt.subplots(figsize=(8, 5))
+plt.plot(x, z)
+plt.grid()
+plt.title("Tangente hiperbólica")
+plt.ylabel("Activación F(x)")
+plt.xlabel("Estímulo x")
+plt.show()
